0dec37138129d7988073b1c9fb2723197da86a6c
Santiago <santiago.salas.sotillo@ucsp.edu.pe>
2023-06-26 22:26:28 -0500

    El final

---
+++ cancion/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+//#include "Nota.h"
+#include "Partitura.h"
+#include <windows.h>
+
+
+using namespace std;
+
+
+int main(){
+
+    //Nota ob(524,1400);
+
+    //ob.getsonido();
+
+
+    Partitura x(28);
+    
+
+   
+
+    x.verificar();
+
+    //x.mostrar();
+
+
+
+
+    x.getsonido();
+
+
+/*
+
+    x.mostrar();
+
+    x.Print();
+
+    x.getcancion();
+
+    x.getSonido();
+
+*/
+
+
+
+
+
+}
